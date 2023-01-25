0aded36c53ffa1afe0149d1ef8308c1be06c3fe4
TopVirusMinus <mwmma5000@gmail.com>
2023-01-25 22:07:02 +0200

    setup sanity client and hid the token

---
+++ ecommerce-nextjs/lib/client.js
@@ -0,0 +1,13 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = sanityClient({
+  projectId: '2to2aaao',
+  dataset: 'production',
+  apiVersion: '25-1-2023',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+})
+
+const builder = imageUrlBuilder(client)
+export const urlFor = (source) => builder.image(source)
