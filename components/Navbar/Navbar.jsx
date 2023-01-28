42fd8147b34888d5e41c768422b2d4955978eb61
TopVirusMinus <mwmma5000@gmail.com>
2023-01-28 17:49:31 +0200

    added account section in the navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { BiLogIn, BiLogOut } from "react-icons/bi";
 import { useStateContext } from "../../context/StateContext";
 import Cart from "../Cart/Cart";
 import Styles from "./Navbar.module.css";
@@ -13,14 +15,35 @@ const Navbar = () => {
       <p className={Styles.logo}>
         <Link href="/">Marzouk Headphones</Link>
       </p>
-      <button
-        type="button"
-        className={Styles.cart_icon}
-        onClick={() => setShowCart((prev) => !prev)}
-      >
-        <AiOutlineShoppingCart />
-        <span className={Styles.cart_item_qty}>{totalQuantities}</span>
-      </button>
+      <div className={Styles.right_Navbar_section}>
+        <div class={Styles.dropdown}>
+          <RiAccountCircleLine size={25} />
+          <div class={Styles.dropdown_content}>
+            <Link href="/" className={Styles.links}>
+              <BiLogIn />
+              <p>
+                Log In<span style={{ opacity: "0" }}>O</span>
+              </p>
+            </Link>
+            <Link href="/" className={Styles.links}>
+              <AiOutlineHeart />
+              <p>Wishlist</p>
+            </Link>
+            <Link href="/" className={Styles.links}>
+              <BiLogOut />
+              <p>Log Out</p>
+            </Link>
+          </div>
+        </div>
+        <button
+          type="button"
+          className={Styles.cart_icon}
+          onClick={() => setShowCart((prev) => !prev)}
+        >
+          <AiOutlineShoppingCart />
+          <span className={Styles.cart_item_qty}>{totalQuantities}</span>
+        </button>
+      </div>
       {showCart && <Cart />}
     </div>
   );
