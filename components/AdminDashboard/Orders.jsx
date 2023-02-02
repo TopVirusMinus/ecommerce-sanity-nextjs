f98586e20049dd881d906bfac73c9a21e210919f
TopVirusMinus <mwmma5000@gmail.com>
2023-02-02 20:41:38 +0200

    added placeholder data to the orders tap

--- components/AdminDashboard/Orders.jsx
+++ components/AdminDashboard/Orders.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const VISIBLE_FIELDS = [
+    "name",
+    "rating",
+    "country",
+    "dateCreated",
+    "isAdmin",
+  ];
+  const { data } = useDemoData({
+    dataSet: "Employee",
+    visibleFields: VISIBLE_FIELDS,
+    rowLength: 200,
+  });
+
+  console.log(data);
+  return (
+    <div style={{ height: "90vh", width: "100%" }}>
+      <DataGrid {...data} components={{ Toolbar: GridToolbar }} />
+    </div>
+  );
 };
 
 export default Orders;
