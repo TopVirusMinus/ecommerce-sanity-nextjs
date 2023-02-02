b75bb477d377120773c5f9595c53ab0514309aea
TopVirusMinus <mwmma5000@gmail.com>
2023-02-02 20:32:03 +0200

    added admin dashboard with sidenavbar

---
+++ components/AdminDashboard/AdminNavBar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+
+const AdminNavBar = ({ children, handlePageClick }) => {
+  const drawerWidth = 240;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {["Dashboard", "Orders"].map((text, index) => (
+            <ListItem
+              onClick={() => handlePageClick((prev) => text)}
+              key={text}
+              disablePadding
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <DashboardIcon /> : <LocalAtmIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+};
+export default AdminNavBar;
---
+++ src/pages/admin-dashboard/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import AdminNavBar from "../../../components/AdminDashboard/AdminNavBar";
+import Dashboard from "../../../components/AdminDashboard/Dashboard";
+import Orders from "../../../components/AdminDashboard/Orders";
+
+const AdminDashboard = () => {
+  const [currPage, setCurrPage] = useState("Dashboard");
+  console.log(currPage);
+  return (
+    <AdminNavBar handlePageClick={setCurrPage}>
+      {currPage === "Dashboard" ? (
+        <Dashboard />
+      ) : currPage === "Orders" ? (
+        <Orders />
+      ) : null}
+    </AdminNavBar>
+  );
+};
+
+export default AdminDashboard;
